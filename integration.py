562ee3fea1a4890c8d1588d39641020b418ce932
hoskillua <47090776+hoskillua@users.noreply.github.com>
2022-01-05 17:19:22 +0200

    kak

--- integration.py
+++ integration.py
@@ -1,66 +1,68 @@
 from re import T
 import skimage.io as io
 from utilities import getSymboles
+from utilities import getGreyImage
 from utilities import predict
 from utilities import classification_mapping
 from utilities import grouping
 from utilities import ApplyOnImage
 
 
+def OverLapping(path, modelPath):
 
-def OverLapping(path,modelPath):
-
-    image = io.imread(path,as_gray=True)
-
-    # io.imshow(image) 
+    image = io.imread(path)
+    # io.imshow(image)
     # io.show()
-    symbolesList = getSymboles(image)
+    if len(image.shape) == 2:
+        greyImage = image
+    else:
+        greyImage = getGreyImage(image)/255.0
+    symbolesList = getSymboles(greyImage)
     features = []
     for symbol in symbolesList:
-        # io.imshow(symbol[0]) 
-        # io.show()
-        prediction = predict(symbol[0],modelPath)
+        io.imshow(symbol[0])
+        io.show()
+        prediction = predict(symbol[0], modelPath)
         prediction = classification_mapping(prediction)
         features.append((prediction, symbol[1], symbol[2]))
     cards = grouping(features)
     print("Cards are:", cards)
     print("Total Number of Cards:", len(cards))
 
-def NonOverLapping(path,modelPath):
+
+def NonOverLapping(path, modelPath):
 
     image = io.imread(path)
 
-    splitedCorners=ApplyOnImage(image)
+    splitedCorners = ApplyOnImage(image)
 
-    cards=[]
+    cards = []
 
     for corner in splitedCorners:
-        # io.imshow(corner[0]/255.0) 
+        # io.imshow(corner[0]/255.0)
         # io.show()
-        # io.imshow(corner[1]/255.0) 
+        # io.imshow(corner[1]/255.0)
         # io.show()
-        p1=predict(corner[0]/255.0,modelPath)
-        p2=predict(corner[1]/255.0,modelPath)
+        p1 = predict(corner[0]/255.0, modelPath)
+        p2 = predict(corner[1]/255.0, modelPath)
 
-        p1=classification_mapping(p1)
-        p2=classification_mapping(p2)
-        cards.append((p1,p2))
+        p1 = classification_mapping(p1)
+        p2 = classification_mapping(p2)
+        cards.append((p1, p2))
 
     print("Cards are:", cards)
     print("Total Number of Cards:", len(cards))
 
 
 def main():
+    path = "Grouping/hand-cards-trump-spades.jpg"
+    modelPath = "Model/model.joblib"
+    AlgoNum = 0
 
-    path="special/7.jpeg"
-    modelPath="Coach-ina/Model/model.joblib"
-    AlgoNum=2
-
-    if(AlgoNum==0 or AlgoNum==2):
-        OverLapping(path,modelPath)
-    if(AlgoNum==1 or AlgoNum==2):
-        NonOverLapping(path,modelPath)
-
+    if(AlgoNum == 0 or AlgoNum == 2):
+        OverLapping(path, modelPath)
+    if(AlgoNum == 1 or AlgoNum == 2):
+        NonOverLapping(path, modelPath)
 
 
 if __name__ == "__main__":
